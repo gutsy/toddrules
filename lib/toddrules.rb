cccfb23fa34e12e67c5569ea03c770ad9a1dd514
Todd Guerra <tguerra@crownpartners.com>
2014-04-17 16:58:18 -0400

    i am ashamed of myself

---
+++ lib/toddrules.rb
@@ -0,0 +1,24 @@
+class ToddRules
+	
+	def self.tommy
+		puts "Tommy once ate pizza out of a trashcan BECAUSE TOMMY"
+	end
+
+	def self.will
+		puts "Will wets the bed at night when no one is looking"
+	end
+
+	def self.james
+		puts "James is ok"
+	end
+
+	def self.adam
+		puts "Adam smells his own farts and everyone knows it"
+	end
+
+	def self.todd 
+		puts "Todd is probably the greatest man to ever live"
+	end
+end
+
+
